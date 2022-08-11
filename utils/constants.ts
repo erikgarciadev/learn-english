017964180f5654e30b070f464856d2c05e897620
Erik Garcia Tacas <erik.fisi16@gmail.com>
2022-08-11 00:39:32 -0500

    fix: correction image no available

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,1 +1,3 @@
-export const urlNoImage = 'https://drive.google.com/uc?export=view&id=1hioORrfxVoPzqSmq9ti8FoqDYG5548sp'
+export const urlNoImage =
+  "/No-Image-Placeholder.png" ||
+  "https://drive.google.com/uc?export=view&id=1hioORrfxVoPzqSmq9ti8FoqDYG5548sp";
